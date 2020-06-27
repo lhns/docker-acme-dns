25cf324e052a823fed050d030fb13b25898d075a
Pierre Kisters <LolHens@users.noreply.github.com>
2020-06-27 21:59:10 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 if [ -n "$ACME_DNS_NS" ]
 then
-  export ACME_DNS_RECORDS=s"\"${ACME_DNS_DOMAIN}. NS ${ACME_DNS_NS}.\",\n$ACME_DNS_RECORDS"
+  export ACME_DNS_RECORDS=$"\"${ACME_DNS_DOMAIN}. NS ${ACME_DNS_NS}.\",\n$ACME_DNS_RECORDS"
 fi
 
 if [ -n "$ACME_DNS_IP" ]
 then
-  export ACME_DNS_RECORDS=s"\"${ACME_DNS_DOMAIN}. A ${ACME_DNS_IP}\",\n$ACME_DNS_RECORDS"
+  export ACME_DNS_RECORDS=$"\"${ACME_DNS_DOMAIN}. A ${ACME_DNS_IP}\",\n$ACME_DNS_RECORDS"
 fi
 
 cat config_template.cfg | envsubst > /etc/acme-dns/config.cfg
